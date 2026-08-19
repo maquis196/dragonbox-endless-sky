009b3fc13087178c6860c23d8493dc14a957c6fd
Christopher May-Townsend <chris@maytownsend.co.uk>
2026-08-19 22:22:50 +0100

    initial commit

---
+++ local_build_dbp.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+set -euo pipefail
+
+VERSION="${1:?Usage: $0 <version - example - 0.22.1>}"
+UPSTREAM_VERSION="${VERSION%-r*}"
+
+PACKAGE_NAME="endless-sky"
+REVISION="${VERSION##*-r}"
+P="${PACKAGE_NAME}-${UPSTREAM_VERSION}"
+
+# Fetch sources
+mkdir -p build
+cd build
+rm -f "v${UPSTREAM_VERSION}.tar.gz"
+wget https://github.com/endless-sky/endless-sky/archive/refs/tags/v0.11.2.tar.gz
+tar zxpvf v${UPSTREAM_VERSION}.tar.gz
+cd endless-sky-${UPSTREAM_VERSION}
+
+# Build # Set options
+cmake .
+make -j2
+
+## Configure meta file
+cat > assets/meta/default.desktop <<EOF
+[Desktop Entry]
+Version=${UPSTREAM_VERSION}
+Type=Application
+Categories=Game;Strategy;
+Name=Endless Sky
+Exec=endless_sky.sh
+Icon=icon.jpg
+X-DBP-Screenshot=teaser1.jpg
+
+[Package Entry]
+Id=endless_sky_maquis196
+Name=Endless Sky
+Arch=armhf
+Exec=endless_sky.sh
+Version=${VERSION}
+Icon=icon.jpg
+EOF
+
+rm -f data.zip gamedata.sqfs
+mksquashfs gamedata gamedata.sqfs -comp xz
+
+cd assets
+zip -r ../data.zip *
+cd ..
+
+cat gamedata.sqfs data.zip > "${P}_maquis196.dbp"
